590eaaa2c63428d8735883a5136fa4a6b9c74727
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-09 10:20:13 -0300

    accounts: starting the accounts project

---
+++ 5_ACCOUNTS/index.js
@@ -0,0 +1,8 @@
+// external modules
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+
+// internal modules
+const fs = require('fs');
+
+console.log('Starting the Accounts project');
