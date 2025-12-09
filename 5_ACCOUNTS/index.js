0bf78f88ec6434eb2fdeec45e92235693342e410
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-09 10:43:34 -0300

    account creation function completed

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -18,7 +18,49 @@ function operations() {
     ])
     .then((answer) => {
         const action = answer['action'];
-        console.log(action);
+        
+        if (action === 'Create Account') {
+            createAccount();
+        }
+    })
+    .catch((err) => console.log(chalk.bgRed(err)));
+}
+
+// create an account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Congratulations on choosing our bank!'));
+    console.log(chalk.green('Set your account options below:'));
+    buildAccount();
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Please provide the account name:'
+        }
+    ])
+    .then((answer) => {
+        const accountName = answer['accountName'];
+
+        console.info(accountName);
+
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('This account already exists, please choose a different name!'));
+            buildAccount();
+            return;
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function (err) {
+            console.log(chalk.bgRed(err));
+        })
+
+        console.log(chalk.green('Congratulations, your account has been successfully created!'));
+        operations();
     })
     .catch((err) => console.log(chalk.bgRed(err)));
 }
