1fdb0dd37cddc3750973a035d1b25b5e33de346d
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-08 22:40:25 -0300

    core modules: path

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const customPath = '/reports/octavio/report1.pdf';
+
+console.log(path.dirname(customPath));
+console.log(path.basename(customPath));
+console.log(path.extname(customPath));
