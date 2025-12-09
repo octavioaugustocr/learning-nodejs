d7b0279d7db9daac635a236bef474f1bddb0ed1c
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-09 10:26:56 -0300

    accounts: inquirer prompt

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -5,4 +5,20 @@ const inquirer = require('inquirer');
 // internal modules
 const fs = require('fs');
 
-console.log('Starting the Accounts project');
+operations();
+
+function operations() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What do you want to do?',
+            choices: ['Create Account', 'Check Balance', 'Deposit', 'Cash Out', 'Logout']
+        }
+    ])
+    .then((answer) => {
+        const action = answer['action'];
+        console.log(action);
+    })
+    .catch((err) => console.log(chalk.bgRed(err)));
+}
