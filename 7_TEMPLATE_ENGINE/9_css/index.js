7047310d916af96843b1564c1d9aab1c609299e9
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-11 11:17:01 -0300

    template engine: css

---
+++ 7_TEMPLATE_ENGINE/9_css/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const port = 3000;
+
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+    console.log(`http://localhost:${port}`);
+});
