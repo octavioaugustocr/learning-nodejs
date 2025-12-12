3387cd2742e1d833ba083576314570b2fe3406a5
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-12 13:19:57 -0300

    node and mysql: where

---
+++ 8_NODE_AND_MYSQL/4_where/index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const mysql = require('mysql2');
+const port = 3000;
+
+const hbs = exphbs.create({ partialsDir: ['views/partial'] });
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    database: 'nodemysql1',
+    user: 'root',
+    password: ''
+});
+
+conn.connect(function(err) {
+    if (err) {
+        console.log(err);
+    }
+
+    app.listen(port, () => {
+        console.log(`Server running on port: ${port}`);
+        console.log(`http://localhost:${port}`);
+    });
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/books', (req, res) => {
+    const sql = `SELECT * FROM books;`;
+
+    conn.query(sql, function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+
+        const books = data;
+        console.log(books);
+
+        res.render('books', { books });
+    });
+});
+
+app.get('/book/:id', (req, res) => {
+    const id = req.params.id;
+
+    const sql = `SELECT * FROM books WHERE (id) = ${id}`;
+
+    conn.query(sql, function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+
+        const book = data[0];
+        console.log(book);
+
+        res.render('book', { book });
+    })
+});
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
+
+    conn.query(sql, function(err) {
+        if (err) {
+            console.log(err);
+        }
+
+        res.render('home');
+    });
+});
