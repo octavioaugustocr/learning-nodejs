be75323379d1fb13d7be2fc5d131e6db1eb8220e
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-12 22:57:42 -0300

    node and mysql: delete

---
+++ 8_NODE_AND_MYSQL/6_delete/commands.md
@@ -0,0 +1,12 @@
+npm init -y
+npx gitignore node
+npm install express express-handlebars mysql2
+npm install --save-dev nodemon
+
+package.json -> scripts -> "start": "nodemon ./index.js localhost 3000"
+
+BOOTSTRAP 5:
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.13.1/font/bootstrap-icons.min.css" />
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
---
+++ 8_NODE_AND_MYSQL/6_delete/index.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const mysql = require('mysql2');
+const port = 3000;
+
+const hbs = exphbs.create({ partialsDir: ['views/partials'] });
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    database: 'nodemysql1',
+    user: 'root',
+    password: ''
+});
+
+conn.connect(function(err) {
+    if (err) {
+        console.log(err);
+    }
+
+    app.listen(port, () => {
+        console.log(`Server running on port: ${port}`);
+        console.log(`http://localhost:${port}`);
+    });
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const sql = `SELECT * FROM books;`
+
+    conn.query(sql, function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+
+        const books = data;
+
+        res.render('home', { books });
+    });
+});
+
+app.get('/book/:id', (req, res) => {
+    const id = req.params.id;
+
+    const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+    conn.query(sql, function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+
+        const book = data[0];
+
+        res.render('book', { book });
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+
+    const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+    conn.query(sql, function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+
+        const book = data[0];
+
+        res.render('editbook', { book });
+    });
+});
+
+app.post('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const sql = `UPDATE books SET title = '${title}', pageqty = ${pageqty} WHERE id = ${id}`;
+    
+    conn.query(sql, function(err) {
+        if (err) {
+            console.log(err);
+        }
+
+        console.log('Update completed successfully!');
+
+        res.redirect('/');
+    });
+});
+
+app.get('/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    const sql = `DELETE FROM books WHERE id = ${id}`;
+
+    conn.query(sql, function(err) {
+        if (err) {
+            console.log(err);
+        }
+
+        console.log(`Book with ID ${id} deleted successfuly!`);
+
+        res.redirect('/');
+    });
+});
